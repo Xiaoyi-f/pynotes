026eca50c9a52eab844887dc23b65bc5a956969b
XiaoYi <15779544219@163.com>
2026-02-12 18:30:03 +0800

    wa

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="XiaoYi"
-
-ENTRYPOINT ["top", "-b"]
---
+++ shell.sh
@@ -0,0 +1,33 @@
+# 基本命令
+# command [-option] [parameter]
+ls -l -h -a
+tree
+pwd
+clear
+cd
+touch
+mkdir -p
+rm -r -f -i
+rmdir
+cp -i -r -v
+mv -i -r -v
+--help
+man cmd
+# > 覆盖
+# >> 追加
+cat
+more
+# | 管道传递数据
+ln -s # 创建软链接
+grep str file -v -n -i # 支持正则
+find . -name "*.txt" # 查找当前目录下所有txt文件
+tar # 压缩和解压缩
+chmod
+whoami
+passwd # 改密码
+sudo
+ssh root@ip
+curl -o file url # 下载文件
+ping ip/域名
+
+
