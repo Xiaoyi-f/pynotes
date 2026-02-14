6ea91cd34d9821143c328629e629d3b830fd2418
XiaoYi <15779544219@163.com>
2026-02-14 22:55:45 +0800

    three

--- algorithm/compute02.py
+++ algorithm/compute02.py
@@ -9,3 +9,11 @@ if __name__ == "__main__":
     nums = input().split(" ")
     nums = list(map(lambda x: int(x.strip()), nums))  # 返回迭代器，不是直接原型操作
     print(handler(0, nums, len) + 1)
+
+# 计动递归
+"""
+总结:
+    发生一次递归动作就计数
+    跳跃问题
+    递归 = 递进 + 回归 
+"""
---
+++ algorithm/compute03.py
@@ -0,0 +1,29 @@
+n = int(input())
+arr = []
+
+
+def handler(start, len):
+    if len > 0:
+        for i in range(len):
+            if i >= 1:
+                print(" ", end="")
+            print(arr[i], end="")
+        print()
+
+    for i in range(start, n + 1):
+        arr.append(i)
+        handler(i + 1, len + 1)
+        arr.pop()
+
+
+if __name__ == "__main__":
+    handler(1, 0)
+
+# 序态递归
+"""
+总结:
+    最后一次递进虽然没有执行有效代码，
+    但是也是递进，回归依旧执行，
+    最后会执行以下arr.pop()这个很容易被忽略
+    递归 = 递进 + 回归
+"""
