4dcd508a956b8f3d7cfb031a28aaadb31af3834a
XiaoYi <15779544219@163.com>
2026-02-20 23:24:25 +0800

    demo

--- bookstore/config.py
+++ bookstore/config.py
@@ -5,4 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db", "database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = 'your-secret-key-change-this-in-production-12345678'
